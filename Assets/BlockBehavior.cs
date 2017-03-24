f7a5d1504afb39284f3a6923089b1db485a9b98b
Viraj Sinha <virajosinha@gmail.com>
2017-03-23 20:30:47 -0700

    bringing things up to date (halfway through pun tutorial)

---
+++ Assets/BlockBehavior.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockBehavior : MonoBehaviour {
+    private Rigidbody2D rb;
+    private RigidbodyConstraints2D originalConstraints;
+
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody2D>();
+        originalConstraints = rb.constraints;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void SetAttached() {
+        Destroy(rb);
+        //rb.constraints = RigidbodyConstraints2D.FreezeAll;
+    }
+    
+    public void SetDetached() {
+        rb = this.gameObject.AddComponent<Rigidbody2D>();
+        rb.constraints = originalConstraints;
+    }
+}
--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -5,18 +5,36 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour {
 
 	public float speed = 5.0f;
+    private PhotonView player;
 
-	// Use this for initialization
-	void Start () {
-		
+    private Vector3 syncStartPosition = Vector3.zero;
+    private Vector3 syncEndPosition = Vector3.zero;
+
+   
+    // Use this for initialization
+    void Start () {
+        player = GetComponent<PhotonView>();
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-		Move ();
-	}
+        if (player.isMine) {
+            InputMovement();
+        }
+    }
 
-	private void Move() {
+    // in an "observed" script:
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info) {
+        if (stream.isWriting) {
+            Vector3 pos = transform.localPosition;
+            stream.Serialize(ref pos);
+        } else {
+            Vector3 pos = Vector3.zero;
+            stream.Serialize(ref pos);  // pos gets filled-in. must be used somewhere
+        }
+    }
+
+    private void InputMovement() {
 		var move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
 
 		transform.position += move * speed * Time.deltaTime;
